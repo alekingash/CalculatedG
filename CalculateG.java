e0442e2c85b0bcfaa28cb74bd23f82fc0a782eb0
alekingash <125488377+alekingash@users.noreply.github.com>
2023-02-15 21:54:23 +0300

    Add files via upload

---
+++ CalculateG.java
@@ -0,0 +1,41 @@
+public class CalculateG {
+    public static Double multiplacation(Double f, double x) {
+        // method for multiplacation
+        return f * z;
+    }
+
+}
+
+public static double add(double f ,double x){
+    //add 2 more methods for powering to square and addation (similar)
+    return f + x}
+
+    public static double square(double f) {
+        // add 2 more methods for powering to square and addation(similar)
+        return Math.pow(f, 2);
+    }
+
+    public static void outline(string message, double result) {
+        // method for printing out a result
+        system.out.println(message + result);
+    }
+
+public static void main(string[]args){
+    //compute the position and velocity of an object with defined methods
+    double gravity=-9.81;
+    //earth gravity in m/s^2
+    double fallingtime=30;
+    double initialvelocity=0.0;
+    double initialposition=0.0;
+    //add the formula for postion and  velocity
+
+    //add output line for velocity(similar to position)
+     double finalposition= addation(multiplacation(f=0.5, gravity)multiplacation(square(fallingtime),gravity));
+     finalposition=addation(multiply(initialvelocity ,fallingtime), addation(initialposition,finalposition));
+     String positionmessage="the object's position after" + fallingtime +"second is";
+     outline(positionmessage, finalposition);
+
+     double finalvelocity =multiply(gravity, fallingtime)+ initialvelocity;
+     string velocitymessage="the objects velocity after" +fallingtime +"second is";
+     outline(velocitymessage, finalvelocity);
+}
